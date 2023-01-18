0a3bfdcd2ef51850e16a45681fa21263c171d2df
user <svvo2002@naver.com>
2023-01-18 23:32:35 +0900

    230118 화면작업 및 api 공부

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -1,15 +1,57 @@
 package com.example.android
 
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //터치 이벤트
+        fun onTouchEvent(event: MotionEvent?): Boolean {
+            when(event?.action){
+                MotionEvent.ACTION_DOWN ->{
+                    Log.d("jin","Touch down")
+                    println("touch down x = ${event.x}, rawX = ${event.rawX}")
+                }
+                MotionEvent.ACTION_UP ->{
+                    Log.d("jin","Touch up")
+                    println("touch up x = ${event.x}, rawX = ${event.rawX}")
+                }
+            }
+            return super.onTouchEvent(event)
+        }
+
+        fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+            when(keyCode){
+                KeyEvent.KEYCODE_BACK -> Log.d("jin","BACK Button")
+                KeyEvent.KEYCODE_VOLUME_UP -> Log.d("jin","Volume up Button")
+                KeyEvent.KEYCODE_VOLUME_DOWN -> Log.d("jin","Volume down Button")
+            }
+
+            return super.onKeyDown(keyCode, event)
+        }
+
+        fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
+            Log.d("jin","onKeyUp")
+            return super.onKeyUp(keyCode, event)
+        }
+
+        // 뒤로가기 이벤트
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                Log.d("Jin","Back Button")
+            }
+        })
+
 
         // 바인딩 객체
         val binging = ActivityMainBinding.inflate(layoutInflater)
@@ -67,3 +109,4 @@ class MainActivity : AppCompatActivity() {
     */
     }
 }
+
